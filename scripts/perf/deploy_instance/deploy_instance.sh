cf0b4697f320cef12caa1dc3fcb610e2d6a0a79e
Chris Llanwarne <cjllanwarne@gmail.com>
2019-05-20 17:59:18 -0400

    Stage deployed instance's startup script

--- scripts/perf/deploy_instance/deploy_instance.sh
+++ scripts/perf/deploy_instance/deploy_instance.sh
@@ -31,6 +31,8 @@ metadata=(
   "CROMWELL_PERF_SCRIPTS_BRANCH=${REPO_BRANCH}"
 )
 
+cp deploy_instance/run_on_instance.sh mnt/
+
 docker run --name perf_gcloud_$BUILD_NUMBER -v "$(pwd)"/mnt:$DOCKER_ETC_PATH --rm google/cloud-sdk:slim /bin/bash -c "\
     gcloud auth activate-service-account --key-file $DOCKER_ETC_PATH/sa.json &&\
 gcloud \
@@ -41,6 +43,6 @@ gcloud \
     create $BUILD_TAG \
     --zone us-central1-c \
     --source-instance-template $INSTANCE_TEMPLATE \
-    --metadata-from-file startup-script=$DOCKER_ETC_PATH/deploy_instance/startup_script.sh \
+    --metadata-from-file startup-script=$DOCKER_ETC_PATH/run_on_instance.sh \
     --metadata \
         $(join ${metadata[@]})"
