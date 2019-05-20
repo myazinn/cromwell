c8c7fa6f5eb578d15263c0743a3968f5574efa8c
Miguel Covarrubias <mcovarr@broadinstitute.org>
2019-05-20 16:00:45 -0400

    dynamic [force ci]

--- src/ci/bin/testCentaurKubernetesPapiV2.sh
+++ src/ci/bin/testCentaurKubernetesPapiV2.sh
@@ -6,13 +6,21 @@ export CROMWELL_BUILD_REQUIRES_SECURE=true
 # Right now these are hardcoded, eventually they should be generated. In order to have the variables rendered in template
 # files they would need to be defined before cromwell::build::setup_common_environment runs, which currently means they
 # would need to be defined before test.inc.sh is loaded (transitively or directly).
-KUBE_CLUSTER_NAME="mlc-k8s-3-2"
-KUBE_CLOUDSQL_CONNECTION_NAME="broad-dsde-cromwell-dev:us-central1:mlc-cloudsql-k8s-experiments"
+#KUBE_CLUSTER_NAME="mlc-k8s-3-2"
+#KUBE_CLOUDSQL_CONNECTION_NAME="broad-dsde-cromwell-dev:us-central1:mlc-cloudsql-k8s-experiments"
 KUBE_CROMWELL_IMAGE="broadinstitute/cromwell:41"
 
 # import in shellcheck / CI / IntelliJ compatible ways
 # shellcheck source=/dev/null
 source "${BASH_SOURCE%/*}/test_kube.inc.sh" || source test_kube.inc.sh
+
+KUBE_CLUSTER_NAME=$(cromwell::kube::generate_gke_cluster_name)
+cromwell::kube::create_gke_cluster ${KUBE_CLUSTER_NAME}
+KUBE_CLOUDSQL_INSTANCE_NAME="$(cromwell::kube::generate_cloud_sql_instance_name)"
+cromwell::kube::create_cloud_sql_instance ${KUBE_CLOUDSQL_INSTANCE_NAME}
+# Get the connectionName for this newly created instance. This is what the Cloud SQL proxies will need for their -instances parameter.
+KUBE_CLOUDSQL_CONNECTION_NAME="$(cromwell::kube::connection_name_for_cloud_sql_instance ${KUBE_CLOUDSQL_INSTANCE_NAME})"
+echo "Cloud SQL connection name is $KUBE_CLOUDSQL_CONNECTION_NAME"
 
 cromwell::kube::create_deployment_templates
 cromwell::kube::create_secrets
@@ -44,22 +52,8 @@ export TEST_CROMWELL_COMPOSE_FILE="${CROMWELL_BUILD_ROOT_DIRECTORY}/scripts/dock
 #    "${TEST_CROMWELL_COMPOSE_FILE}" \
 #    "${CROMWELL_BUILD_CENTAUR_TEST_RENDERED}"
 
-# Temporarily turning GKE cluster stuff off as it's unnecessary for testing Cloud SQL stuff.
-#
-# KUBE_CLUSTER_NAME=$(cromwell::kube::generate_gke_cluster_name)
-# cromwell::kube::create_gke_cluster ${KUBE_CLUSTER_NAME}
 # TODO Move this to the "cleanup" section of the script once there is also a "do real work" section.
 # cromwell:kube::destroy_gke_cluster ${KUBE_CLUSTER_NAME}
-
-# Temporarily turning Cloud SQL stuff off as it's unnecessary for testing GCR stuff.
-#KUBE_CLOUDSQL_INSTANCE_NAME="$(cromwell::kube::generate_cloud_sql_instance_name)"
-#echo "Cloud SQL instance name is $KUBE_CLOUDSQL_INSTANCE_NAME"
-#
-#cromwell::kube::create_cloud_sql_instance ${KUBE_CLOUDSQL_INSTANCE_NAME}
-## Get the connectionName for this newly created instance. This is what the Cloud SQL proxies will need for their -instances parameter.
-#KUBE_CLOUDSQL_CONNECTION_NAME="$(cromwell::kube::connection_name_for_cloud_sql_instance ${KUBE_CLOUDSQL_INSTANCE_NAME})"
-#echo "Cloud SQL connection name is $KUBE_CLOUDSQL_CONNECTION_NAME"
-#
 
 ## TODO Move this to the "cleanup" section of the script once there is also a "do real work" section.
 #cromwell::kube::destroy_cloud_sql_instance ${KUBE_CLOUDSQL_INSTANCE_NAME}
